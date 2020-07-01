8487a86a1a48326e3d74ba4e8425364f464741ff
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-07-01 20:54:15 +1000

    feat(home): update page to create a pdf

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,37 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, ReactElement } from "react"
+import useFetch from "../hooks/useFetch"
+import documentData from "../data/documentData"
+import b64toBlob from "b64-to-blob"
 
-var documentData = {
-	content: [
-		{
-			text: 'This is a header, using header style',
-			style: 'header'
-		},
-		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam.\n\n',
-		{
-			text: 'Subheader 1 - using subheader style',
-			style: 'subheader'
-		},
-		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.',
-		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.',
-		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.\n\n',
-		{
-			text: 'Subheader 2 - using subheader style',
-			style: 'subheader'
-		},
-		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.',
-		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.\n\n',
-		{
-			text: 'It is possible to apply multiple styles, by passing an array. This paragraph uses two styles: quote and small. When multiple styles are provided, they are evaluated in the specified order which is important in case they define the same properties',
-			style: ['quote', 'small']
-		}
-	],
-	styles: {
-		header: {
-			fontSize: 18,
-			bold: true
-		},
-		subheader: {
-			fontSize: 15,
-			bold: true
-		},
-		quote: {
-			italics: true
-		},
-		small: {
-			fontSize: 8
-		}
-	}
-}
 console.log(JSON.stringify(documentData, null, 2))
 
+const buttonClassName = "relative block w-full sm:w-auto border border-transparent px-6 py-3 text-base leading-6 font-semibold leading-snug bg-gray-900 text-white rounded-md shadow-md transition ease-in-out duration-150"
+
 export default function Home() {
-  const [loading, setLoading] = useState(false)
+	const [data, loading, performFetch] = useFetch("https://letspdf.vercel.app/api/make")
+	const [pdf, setPdf] = useState<Blob>()
+
+	useEffect(() => {
+		if (data?.message) {
+			const blob = b64toBlob(data.message, "application/pdf")
+			setPdf(blob)
+		}
+	}, [data, setPdf])
+
+	const generatePDF = async (): Promise<void> => {
+		setPdf(undefined)
+		const headers = new Headers()
+		headers.append("Content-Type", "application/json")
+		const json = JSON.stringify({
+			document: documentData,
+		})
+
+		await performFetch({
+			method: "POST",
+			headers,
+			body: json
+		})
+	}
 
   return (
     <div className="flex flex-auto items-center flex-col">
@@ -58,10 +44,32 @@ export default function Home() {
       </div>
       <p className="mt-2 italics text-gray-500">For the full object, see the console</p>
       <div className="mt-5">
-        <button
-          className="relative sm:mt-0 sm:h-auto block w-full sm:w-auto border border-transparent px-6 py-3 text-base leading-6 font-semibold leading-snug bg-gray-900 text-white rounded-md shadow-md hover:bg-gray-800 focus:outline-none focus:bg-gray-800 transition ease-in-out duration-150 hover:bg-gray-600"
-        >Generate PDF</button>
-      </div>
+        <Button
+					disabled={loading}
+					onClick={generatePDF}
+					type="button"
+				>{loading ? "Loading..." : "Generate PDF"}</Button>
+				{
+					pdf && (
+						<a
+							download
+							href={URL.createObjectURL(pdf)}
+							target="_blank"
+							rel="noopener noreferrer"
+							className={`${buttonClassName} bg-blue-600 mt-3`}
+						>
+							Download PDF
+						</a>
+					)
+				}
+			</div>
     </div>
   )
 }
+
+const Button = ({children, ...rest}: any): ReactElement => (
+	<button
+		className={buttonClassName}
+		{...rest}
+	>{children}</button>
+)
